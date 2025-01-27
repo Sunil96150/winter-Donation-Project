493db10a30bc713d511418075c9842e62d7b5d27
Sunil96150 <shrabon.roy986@gmail.com>
2025-01-27 22:27:02 +0600

    add the images

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom"
 import router from "./routes/router";
-import AuthProvider from "./providers/Authprovider";
+import AuthProvider from "../providers/AuthProvider";
 
-// import Aos from "aos";
-// import "aos/dist/aos.css";
-
-// AOS.init();
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
