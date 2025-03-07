80305759fbf20f8ae8998b82de393077d4802cc6
Sunil96150 <shrabon.roy986@gmail.com>
2025-03-07 22:17:04 +0600

    added the login form

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom"
@@ -7,9 +7,9 @@ import AuthProvider from "./providers/AuthProvider";
 
 
 createRoot(document.getElementById("root")).render(
-    <StrictMode>
+   
         <AuthProvider>
             <RouterProvider router={router} />
         </AuthProvider>
-    </StrictMode>
+    
 );
